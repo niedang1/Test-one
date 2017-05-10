88990376c14c019563c3c5429bf1afdc6157f0ad
Charlie <charlie@charliededevice.local>
2017-05-10 18:01:38 +0800

    just init

---
+++ test one/test oneTests/test_oneTests.swift
@@ -0,0 +1,36 @@
+//
+//  test_oneTests.swift
+//  test oneTests
+//
+//  Created by Charlie on 2017/5/10.
+//  Copyright © 2017年 Charlie. All rights reserved.
+//
+
+import XCTest
+@testable import test_one
+
+class test_oneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
